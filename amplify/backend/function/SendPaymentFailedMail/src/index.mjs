f763d18cf0bd9d73af214408d98b941058f6904d
JothivananM <jothivaanan.madheswaran@nipurnait.com>
2024-02-19 10:41:10 +0530

    new backend

--- amplify/backend/function/SendPaymentFailedMail/src/index.mjs
+++ amplify/backend/function/SendPaymentFailedMail/src/index.mjs
@@ -1,21 +1,53 @@
+import AWS from 'aws-sdk';
 import { unmarshall } from '@aws-sdk/util-dynamodb';
 import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 const sesClient = new SESClient({ region: process.env.REGION });  
 const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: process.env.REGION }); // desired AWS region.
+const PaymentDetailsTableName = `PaymentDetails-4mqwuuijsrbx5p6qtibxxchbsq-dev`;
 
 export const handler = async (event) => {
+  
+  try{
           const GETPaymentFailedDetails = await getPaymentFailedDetails();
           console.log("GETPaymentFailedDetails :",GETPaymentFailedDetails);
+          
+          for (const GETPaymentFailedDetails of GETPaymentFailedDetails) {
+      const sendEmailResponse = await sendFailedPaymentEmail(GETPaymentFailedDetails);
+      console.log(`Email sent to ${GETPaymentFailedDetails.Email}. Response:`, sendEmailResponse);
+      
+      if (
+        GETPaymentFailedDetails.BeforePaymentSAPstatus === 'Failed' &&
+        GETPaymentFailedDetails.AfterPaymentSAPstatus === 'Failed' &&
+        GETPaymentFailedDetails.SAPErrorMessage !== ''
+      ) {
+        const sapUpdateEmailResponse = await sendFailedSAPUpdateEmail(GETPaymentFailedDetails);
+        console.log(`SAP Update Email sent to ${GETPaymentFailedDetails.Email}. Response:`, sapUpdateEmailResponse);
+      }
+    }
+    return { statusCode: 200, 
+                  body: 'Emails sent successfully.' 
+      
+           };
+           
+  }catch (error) {
+        console.error('Error:', error);
 
-          const SendFailedPaymentEmail = await sendFailedPaymentEmail();
-          console.log("GETPaymentFailedDetails :",SendFailedPaymentEmail);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' }),
+        };
+        
+    } 
   async function getPaymentFailedDetails() {
 
     const params = {
-      TableName: OrganizationDetailsTableName,
-      FilterExpression: "#PaymentStatus = :Failed",
+      TableName: PaymentDetailsTableName,
+      FilterExpression: "#PaymentStatus = :Failed ,#SAPErrorMessage = :!== <empty> ,AfterPaymentSAPstatus = :Failed ,#BeforePaymentSAPstatus = :Failed",
       ExpressionAttributeNames: {
-        "#PaymentStatus": "Failed",
+      "#PaymentStatus": "PaymentStatus",
+      "#SAPErrorMessage": "SAPErrorMessage",
+      "#AfterPaymentSAPstatus": "AfterPaymentSAPstatus",
+      "#BeforePaymentSAPstatus": "BeforePaymentSAPstatus",
       },
       ExpressionAttributeValues: {
         ":PaymentStatus": "Failed",
@@ -34,13 +66,15 @@ export const handler = async (event) => {
       throw err; // You may want to handle errors in a better way depending on your application needs
     }
   }
+  
   async function sendFailedPaymentEmail(getPaymentFailedDetails){
+    let getDBData = unmarshall(getPaymentFailedDetails);
     let responseBody,subjectContent,textContent,status;
-    if(getPaymentFailedDetails.Status == "Failed"){
+    if(getDBData.Status == "Failed"){
         responseBody =`<span>Unfortunately, the payment has failed.<span>`;
-        subjectContent= `${getPaymentFailedDetails.Description}`;
-        textContent = `Your Payment Failed - TransactionID #${getPaymentFailedDetails.TransID}`;
-        status = `${getPaymentFailedDetails.Status}`;
+        subjectContent= `${getDBData.Description}`;
+        textContent = `Your Payment Failed - TransactionID #${getDBData.TransID}`;
+        status = `${getDBData.Status}`;
     }
     
     const params = { 
@@ -58,10 +92,10 @@ export const handler = async (event) => {
                                     <span style="display: inline-block;font-size: 20px;color: #000000;font-weight:bold">${status}</span>
                                   </div>
                                   <div>
-                                    <span style="display: inline-block;font-size: 16px"">TransactionID #${getPaymentFailedDetails.TransID}</span>
+                                    <span style="display: inline-block;font-size: 16px"">TransactionID #${getDBData.TransID}</span>
                                     </div>
                                   </div>
-                                  <div style=" color : #F9AA33">Hello ${getPaymentFailedDetails.FirstName} ${getPaymentFailedDetails.LastName}
+                                  <div style=" color : #F9AA33">Hello ${getDBData.FirstName} ${getDBData.LastName}
                                   </div>
                                   <div style="margin-top:20px">${responseBody}</div>
                                   <div style="margin-top: 50px">
@@ -86,10 +120,85 @@ export const handler = async (event) => {
       
       console.log("Params : ",params);
       
+    try {
       let response = await sesClient.send(new SendEmailCommand(params));
-         
-      console.log("Response : ",response);
-  
+      console.log("Response : ", response);
       return response;
+    } catch (error) {
+      console.error("Error sending email:", error);
+      throw error;
+    }
   }
+  
+  async function sendFailedSAPUpdateEmail(getPaymentFailedDetails) {
+  let getDBData = unmarshall(getPaymentFailedDetails);
+  let responseBody, subjectContent, textContent, status;
+
+  if (
+      getDBData.BeforePaymentSAPstatus === "Failed" &&
+      getDBData.AfterPaymentSAPstatus === "Failed" &&
+      getDBData.SAPErrorMessage !== "") {
+
+    responseBody = `<span>Unfortunately, the SAP update has failed.<span>`;
+    subjectContent = `${getDBData.SAPErrorMessage}`;
+    textContent = `SAP Update Failed - TransactionID #${getDBData.TransID}`;
+    status = `${getDBData.Status}`;
+
+    const params = {
+      Destination: { ToAddresses: [getDBData.Email] },
+      Message: {
+        Body: {
+          Html: {
+            Charset: 'UTF-8',
+            Data: `
+            <html>
+              <body>
+                <div>
+                  <div style="text-align: right;">
+                    <div>
+                      <span style="display: inline-block;font-size: 20px;color: #000000;font-weight:bold">${status}</span>
+                    </div>
+                    <div>
+                      <span style="display: inline-block;font-size: 16px"">TransactionID #${getDBData.TransID}</span>
+                    </div>
+                  </div>
+                  <div style=" color : #F9AA33">Hello ${getDBData.FirstName} ${getDBData.LastName}
+                  </div>
+                  <div style="margin-top:20px">${responseBody}</div>
+                  <div style="margin-top: 50px">
+                    <!-- Add additional content or styling here if needed -->
+                  </div>
+                </div>
+              </body>
+            </html>`,
+          },
+        },
+        Text: {
+          Charset: 'UTF-8',
+          Data: `${textContent}`,
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: `${subjectContent}`,
+      },
+      Source: 'meikandan.kg@nipurnait.com',
+    };
+
+    console.log("Params : ", params);
+
+    try {
+      let response = await sesClient.send(new SendEmailCommand(params));
+      console.log("Response : ", response);
+      return response;
+    } catch (error) {
+      console.error("Error sending email:", error);
+      throw error;
+    }
+  }
+  // If conditions are not met, return null or handle as needed
+  return null;
+}
+
+  
 };
